ea997670c156a92ba6c440bab9570841b133970c
WaShindeiru <franekdata@gmail.com>
2024-06-17 11:10:57 +0200

    add FileAccess test

---
+++ src/test/java/flashcards/utils/FileAccessTest.java
@@ -0,0 +1,35 @@
+package flashcards.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class FileAccessTest {
+
+   private static String filePath = "./src/test/resources/test.txt";
+   private static FileAccess fileAccess = new FileAccess();
+
+   @Test
+   void writeAndReadFromFile() {
+      String message = "test";
+      try {
+         fileAccess.writeToFile(filePath, message);
+      } catch (IOException e) {
+         fail(e.getMessage());
+      }
+
+      String result = null;
+      try {
+         result = fileAccess.readFromFile(filePath).trim();
+      } catch (IOException e) {
+         fail(e.getMessage());
+      }
+
+      assertNotNull(result);
+      assertThat(result).isEqualTo(message);
+   }
+}
